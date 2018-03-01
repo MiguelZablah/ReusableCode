fc071bdbc1a1aedd3dbd3176707a4dfb4cb8b3ed
Zablah <miguel.ramirez@diginteligencia.com>
2018-03-01 14:31:49 -0600

    videoPlayer added

--- gulpfile.js
+++ gulpfile.js
@@ -46,7 +46,46 @@ gulp.task('localStorageBuild', function() {
             presets: ['env']
         }))
         .pipe(minify())
-        .pipe(gulp.dest('dist/localStorage'));
+        .pipe(gulp.dest('dist/localStorage/'));
 });
 
-gulp.task('default', ['likeAndRatingBuildJS', 'likeAndRatingBuildCSS', 'cookiesBuild', 'localStorageBuild']);
+// Video Player for download confirm an category use
+gulp.task('videoPlayerBuildJS', function() {
+    gulp.src('src/videoPlayer/*.js')
+        .pipe(concat('videoPlayer.js'))
+        .pipe(stripJsComments())
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(minify())
+        .pipe(gulp.dest('dist/videoPlayer/'));
+});
+
+// All video Player PlugIns
+gulp.task('videoPlayerPlugInsBuildJS', function() {
+    gulp.src('src/videoPlayerPlugIns/js/*.js')
+        .pipe(concat('videoPlayerPlugIns.js'))
+        .pipe(stripJsComments())
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(minify())
+        .pipe(gulp.dest('dist/videoPlayerPlugIns/'));
+});
+gulp.task('videoPlayerPlugInsBuildCSS', function() {
+    gulp.src('src/videoPlayerPlugIns/css/*.css')
+        .pipe(concat('videoPlayerPlugIns.css'))
+        .pipe(stripCssComments())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('dist/videoPlayerPlugIns/'));
+});
+
+gulp.task('default', [
+    'likeAndRatingBuildJS', 
+    'likeAndRatingBuildCSS', 
+    'cookiesBuild', 
+    'localStorageBuild',
+    'videoPlayerBuildJS',
+    'videoPlayerPlugInsBuildJS',
+    'videoPlayerPlugInsBuildCSS'
+]);
